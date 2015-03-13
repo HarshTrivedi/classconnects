b9a70852e02e0afc2835106e709715bc1972c338
Harsh Trivedi <harshjtrivedi94@gmail.com>
2015-03-13 08:51:16 +0530

    add all the routes corresponding to which controllers and actions have to be made.

--- config/routes.rb
+++ config/routes.rb
@@ -10,9 +10,42 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'landings#index'
 
+  get 'colleges/:id/content'  => 'college#show_content'         , as: :college_content
+  get 'colleges/:id/peers'    => 'college#show_peers'           , as: :college_peers
+  get 'colleges/:id/courses'  => 'college#show_courses'         , as: :college_courses
+  get 'colleges/:id/discuss'  => 'college#show_discussion'      , as: :college_discussion
+
+
+  get 'courses/:id/content'  => 'courses#show_content'          , as: :course_content
+  get 'courses/:id/peers'    => 'courses#show_peers'            , as: :course_peers
+  get 'courses/:id/discuss'  => 'courses#show_discussion'       , as: :course_discussion
+
+
+  get 'buckets/:id/content'  => 'buckets#show_content'          , as: :bucket_content
+  get 'buckets/:id/details'  => 'buckets#show_details'          , as: :bucket_details
+  get 'buckets/:id/edit_details'  => 'buckets#edit_details'     , as: :edit_bucket_details
+  put 'buckets/:id'          => 'buckets#update_details'        , as: :update_bucket_details  
+
+
+  get 'folders/:id/content'       => 'folders#show_content'     , as: :folder_content
+  get 'folders/:id/details'       => 'folders#show_details'     , as: :folder_details
+  get 'folders/:id/edit_details'  => 'folders#edit_details'     , as: :edit_folder_details
+  put 'folders/:id'               => 'folders#update_details'   , as: :update_folder_details  
+
+
+  get 'files/:id/details'       => 'files#show_details'         , as: :file_details
+  get 'files/:id/edit_details'  => 'files#edit_details'         , as: :edit_file_details
+  put 'files/:id'               => 'files#update_details'       , as: :update_file_details  
+
+
+  get '/my_uploads'             => 'profiles#uploads'                   , as: :uploads
+  get '/my_downloads'           => 'profiles#downloads'                 , as: :downloads
+  get '/profile'                => 'profiles#profile'                   , as: :profile
+  get '/enrolled_courses'       => 'profiles#enrolled_courses'          , as: :enrolled_courses
+  get '/favorite_courses'       => 'profiles#favorite_courses'          , as: :favorite_courses
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
