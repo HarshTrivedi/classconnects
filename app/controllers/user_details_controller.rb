08979586ad7cbe0412b47f9b8c3eddd766ec442c
Harsh Trivedi <harshjtrivedi94@gmail.com>
2015-03-13 09:13:49 +0530

    Add UserDetails controller

---
+++ app/controllers/user_details_controller.rb
@@ -0,0 +1,16 @@
+class UserDetailsController < ApplicationController
+  def uploads
+  end
+
+  def downloads
+  end
+
+  def profile
+  end
+
+  def enrolled_courses
+  end
+
+  def favorite_courses
+  end
+end
