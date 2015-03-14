1fc7d083db39ab6922cfe2a1934492010bf18d45
Harsh Trivedi <harshjtrivedi94@gmail.com>
2015-03-14 11:48:48 +0530

    add awesome_print_helper in Application Helper to dump all objects prettily in html page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+
+	def awesome_print_html(object)
+		(ap object , {:html => true , :color => { :array  => :black } } ).gsub(/style="/ , 'style="background-color:transparent;').html_safe
+	end
+
 end
