9321d0799f5cb28c2d57584a16f2fb8e8ad64f2d
Harsh Trivedi <harshjtrivedi94@gmail.com>
2015-03-14 12:47:18 +0530

    add some before filters in course controller to restric access

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,10 +1,20 @@
 class CoursesController < ApplicationController
-  def show_content
+  before_action :authenticate_user!
+  before_filter :course_exists
+
+
+  #BEFORE FILTER methods
+  private
+
+  def course_exists
+      course_exists = Course.exists?(params[:id])
+      if not course_exists
+        render_404
+        return false
+      else
+        return true
+      end
   end
 
-  def show_peers
-  end
 
-  def show_discussion
-  end
 end
