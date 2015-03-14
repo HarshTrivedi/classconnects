8fdfb9626892c5b813ce40af3f82c77a8b663963
Harsh Trivedi <harshjtrivedi94@gmail.com>
2015-03-14 12:48:29 +0530

    add show_content , show_user and show_discussion  action and its corresponding template in courses_controller

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -2,6 +2,26 @@ class CoursesController < ApplicationController
   before_action :authenticate_user!
   before_filter :course_exists
 
+  def show_content
+    course_id = params[:id]
+    course = Course.find_by_id(course_id)
+    @message = "Course specific buckets"    
+    @buckets = course.buckets.page(params[:page])
+  end
+
+  def show_users
+    course_id = params[:id]
+    course = Course.find_by_id(course_id)
+    @message = "Course specific buckets"    
+    @users = course.enrolled_users.page(params[:page])
+  end
+
+  def show_discussion
+    course_id = params[:id]
+    course = Course.find_by_id(course_id)
+    @message = "Course specific buckets"    
+    @comments = course.comments.page(params[:page])
+  end
 
   #BEFORE FILTER methods
   private
