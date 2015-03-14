524495c32b9c2fd80fd7a56a38aa1ec9e7ef81ea
Harsh Trivedi <harshjtrivedi94@gmail.com>
2015-03-14 13:30:09 +0530

    add before filter ; code for actions 'uploads' , 'downloads' , 'enrolled_course' and 'favorite_courses'  and their corresponding view

--- app/controllers/user_details_controller.rb
+++ app/controllers/user_details_controller.rb
@@ -1,16 +1,31 @@
 class UserDetailsController < ApplicationController
+  before_action :authenticate_user!
+
   def uploads
+    @uploads = current_user.uploaded_buckets.page(params[:page])
+
   end
 
   def downloads
+    @downloads = current_user.downloaded_buckets.page(params[:page])
   end
 
   def profile
   end
 
+
+  #Need link to reach here (In courses partial)
   def enrolled_courses
+    @enrolled_courses = current_user.enrolled_courses.page(params[:page])
   end
 
+
+  #Need link to reach here (In courses partial)
   def favorite_courses
+    @favorite_courses = current_user.favorite_courses.page(params[:page])
   end
+
+
+
+
 end
