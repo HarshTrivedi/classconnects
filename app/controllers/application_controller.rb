b686c6e781a6762aeed4cf7b80c2a4efcecb5c1f
Harsh Trivedi <harshjtrivedi94@gmail.com>
2015-03-14 11:55:38 +0530

    add render_404 method that can be used in any action of any control :)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def render_404
+    respond_to do |format|
+      format.html { render :file => "/public/404", :layout => false, :status => :not_found }
+      format.any  { head :not_found }
+    end
+  end
+
   private
   
   #-> Prelang (user_login:devise)
