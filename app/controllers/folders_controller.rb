f405405cd82ac25d3cdc27791701c15fdd52f4e5
Harsh Trivedi <harshjtrivedi94@gmail.com>
2015-03-14 13:21:03 +0530

    Add some before filters in folders controller; add actions: 'show_content' , 'show_details' , 'edit_details' , 'update_details' and the required views

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -1,13 +1,54 @@
 class FoldersController < ApplicationController
+  before_action :authenticate_user!
+  before_filter :folder_exists
+
   def show_content
+    folder_id = params[:id]
+    folder = Folder.find_by_id(folder_id)
+    @message = "Folder specific folders and documents"    
+    @folders = folder.folders.page(params[:page])
+    @documents = folder.documents.page(params[:page])
   end
 
   def show_details
+    folder_id = params[:id]
+    @message = "Folder itself"        
+    @folder = Folder.find_by_id(folder_id)
   end
 
   def edit_details
+    folder_id = params[:id]
+    @message = "Folder form should be added"
+    @folder = Folder.find_by_id(folder_id)
+
+    ### Form will have to be shown for this
+
   end
 
   def update_details
+    folder_id = params[:folder][:id]
+    folder = Folder.find_by_id(folder_id)
+    folder.update_attributes( folder_params )
+    redirect_to folder_details_path(folder_id)
   end
+
+  #BEFORE FILTER methods
+  private
+
+  def folder_exists
+      folder_exists = Folder.exists?(params[:id])
+      if not folder_exists
+        render_404
+        return false
+      else
+        return true
+      end
+  end
+
+  #PERMITTING mass assignment
+  def folder_params
+    params.require(:folder).permit(:name )
+  end
+
+
 end
