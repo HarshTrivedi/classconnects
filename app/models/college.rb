cb5903e7afc7e5975d6d283aab53952a2a4524c2
Harsh Trivedi <harshjtrivedi94@gmail.com>
2015-03-14 12:07:30 +0530

    add model method 'courses' and 'courses_by_branch'

--- app/models/college.rb
+++ app/models/college.rb
@@ -23,6 +23,25 @@ class College < ActiveRecord::Base
   end
 
 
+  def courses
+  	all_college_branch_pairs = college_branch_pairs.flatten
+  	course_ids = all_college_branch_pairs.map{|college_branch_pair| college_branch_pair.courses }.flatten.map(&:id)
+    courses = Course.where(:id => course_ids)
+    return courses
+  end
+
+  def courses_by_branch(branch_id)
+    college_branch_pair = CollegeBranchPair.where(:college_id => self.id , :branch_id => branch_id).first
+    if college_branch_pair
+      course_ids = college_branch_pair.courses.map(&:id)
+      courses = Course.where(:id => course_ids)
+      return courses
+    else
+      return nil
+    end
+  end
+
+
 
 
 
