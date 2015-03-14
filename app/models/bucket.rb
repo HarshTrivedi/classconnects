88005d71621cdb5141d1dece1ede9a57bfca747a
Harsh Trivedi <harshjtrivedi94@gmail.com>
2015-03-14 12:03:34 +0530

    Fix problem. Bucket didn't have has_many association with document.

--- app/models/bucket.rb
+++ app/models/bucket.rb
@@ -2,6 +2,7 @@ class Bucket < ActiveRecord::Base
   belongs_to :category
   belongs_to :course
   has_many :folders
+  has_many :documents
   has_many :uploads
   has_many :downloads
   has_many :comments, as: :commentable
