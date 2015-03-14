1f38521c38b5ed7e6fa5c1bf8d5395e5b6928ff7
Harsh Trivedi <harshjtrivedi94@gmail.com>
2015-03-14 13:22:59 +0530

    Add some before filters in documents controller; add actions: 'show_details' , 'edit_details' , 'update_details' and the required views

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,10 +1,44 @@
 class DocumentsController < ApplicationController
+  paginates_per 10
+  before_action :authenticate_user!
+  before_filter :document_exists
+
   def show_details
+    document_id = params[:id]
+    @message = "Document itself"        
+    @document = Document.find_by_id(document_id)
   end
 
   def edit_details
+    document_id = params[:id]
+    @message = "Document form should be added here"
+    @document = Document.find_by_id(document_id)
   end
 
   def update_details
+    document_id = params[:document][:id]
+    document = Document.find_by_id(document_id)
+    document.update_attributes( document_params )
+    redirect_to document_details_path(document_id)
   end
+
+  #BEFORE FILTER methods
+  private
+
+  def document_exists
+      document_exists = Document.exists?(params[:id])
+      if not document_exists
+        render_404
+        return false
+      else
+        return true
+      end
+  end
+
+  #PERMITTING mass assignment
+  def document_params
+    params.require(:document).permit(:name )
+  end
+
+
 end
