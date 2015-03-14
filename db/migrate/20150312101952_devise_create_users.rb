a042fbc505aa1ddf786ba3c4c44897f6b64998be
Harsh Trivedi <harshjtrivedi94@gmail.com>
2015-03-14 12:01:53 +0530

    Make CollegeBranchPair to have one to many with User; user and college_branch_pair also yet have to be changed with the associations

--- db/migrate/20150312101952_devise_create_users.rb
+++ db/migrate/20150312101952_devise_create_users.rb
@@ -30,6 +30,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
+      t.references :college_branch_pair, index: true
 
       t.timestamps null: false
     end
