e83cab76253179f582d559ff2f88506284ff6ee1
Harsh Trivedi <harshjtrivedi94@gmail.com>
2015-03-14 12:43:06 +0530

    add some before filters in college_controller to prevent invalid request

--- app/controllers/colleges_controller.rb
+++ app/controllers/colleges_controller.rb
@@ -1,13 +1,49 @@
 class CollegesController < ApplicationController
-  def show_content
+  before_action :authenticate_user!
+  before_filter :college_exists
+  before_filter :branch_exists_if_passed 
+  before_filter :branch_passed , :only => [:show_discussion]
+
+
+ 
+
+
+  #BEFORE FILTER methods
+  private
+
+  def college_exists
+      college_exists = College.exists?(params[:id])
+      if not college_exists
+        render_404
+        return false
+      else
+        return true
+      end
   end
 
-  def show_peers
+  def branch_passed
+      branch_exists = Branch.exists?(params[:id])
+      if not branch_exists
+        render_404
+        return false
+      else
+        return true
+      end
   end
 
-  def show_courses
+  def branch_exists_if_passed
+      branch_id = params[:branch_id]
+      return true if branch_id.nil?
+      branch_exists = Branch.exists?(branch_id)
+      if not branch_exists
+        render_404
+        return false
+      else
+        return true
+      end
+
   end
 
-  def show_discussion
-  end
+
+
 end
