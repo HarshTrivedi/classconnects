05426f8a7ffa804d96d712964ec638e10f38ccc0
Harsh Trivedi <harshjtrivedi94@gmail.com>
2015-03-14 12:40:16 +0530

    remove unrequired model and fix a migration on creating document.

--- app/models/user_college_branch_enrollment.rb
+++
@@ -1,4 +0,0 @@
-class UserCollegeBranchEnrollment < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :college_branch_pair
-end
--- db/migrate/20150312102036_create_documents.rb
+++ db/migrate/20150312102036_create_documents.rb
@@ -2,6 +2,8 @@ class CreateDocuments < ActiveRecord::Migration
   def change
     create_table :documents do |t|
       t.references :folder, index: true
+      t.references :bucket, index: true
+      t.string :name
       t.string :cloud_path
 
       t.timestamps
