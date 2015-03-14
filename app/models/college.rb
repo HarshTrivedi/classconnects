7ac8ec73162788178fff3a259f63a77942da54b5
Harsh Trivedi <harshjtrivedi94@gmail.com>
2015-03-14 12:07:08 +0530

    add model method 'buckets' and 'buckets_by_branch'

--- app/models/college.rb
+++ app/models/college.rb
@@ -2,4 +2,28 @@ class College < ActiveRecord::Base
   has_many :college_branch_pairs
   has_many :branches, through: :college_branch_pairs
 
+  def buckets
+  	all_college_branch_pairs = self.college_branch_pairs
+  	courses = all_college_branch_pairs.map{|college_branch_pair| college_branch_pair.courses }.flatten
+  	bucket_ids = courses.map{|course| course.buckets }.flatten.map(&:id)
+    buckets = Bucket.where(:id => bucket_ids)
+  	return buckets
+  end
+
+  def buckets_by_branch(branch_id)
+    college_branch_pair = CollegeBranchPair.where(:college_id => self.id , :branch_id => branch_id).first
+    if college_branch_pair
+      courses = college_branch_pair.courses rescue nil
+      bucket_ids = courses.map{|course| course.buckets }.flatten.map(&:id) rescue nil
+      buckets = Bucket.where(:id => bucket_ids)
+      return buckets
+    else
+      return nil
+    end
+  end
+
+
+
+
+
 end
