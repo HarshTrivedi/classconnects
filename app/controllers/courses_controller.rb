6ef1c18d06a41a2238f2ebb1950e89988aa5af9e
Harsh Trivedi <harshjtrivedi94@gmail.com>
2015-03-15 22:48:15 +0530

    Add logged_in layout to course controller and make some local variables instance so that they can be accessed in the views

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,26 +1,27 @@
 class CoursesController < ApplicationController
+  layout "logged_in"
   before_action :authenticate_user!
   before_filter :course_exists
 
   def show_content
     course_id = params[:id]
-    course = Course.find_by_id(course_id)
+    @course = Course.find_by_id(course_id)
     @message = "Course specific buckets"    
-    @buckets = course.buckets.page(params[:page])
+    @buckets = @course.buckets.page(params[:page])
   end
 
   def show_users
     course_id = params[:id]
-    course = Course.find_by_id(course_id)
+    @course = Course.find_by_id(course_id)
     @message = "Course specific buckets"    
-    @users = course.enrolled_users.page(params[:page])
+    @users = @course.enrolled_users.page(params[:page])
   end
 
   def show_discussion
     course_id = params[:id]
-    course = Course.find_by_id(course_id)
+    @course = Course.find_by_id(course_id)
     @message = "Course specific buckets"    
-    @comments = course.comments.page(params[:page])
+    @comments = @course.comments.page(params[:page])
   end
 
   #BEFORE FILTER methods
