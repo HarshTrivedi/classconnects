d9f05d06ce8884738230018e3ebe1073ccc878e1
Harsh Trivedi <harshjtrivedi94@gmail.com>
2015-03-15 23:30:51 +0530

    Add 2 model methods : to get bucket uploader and bucket downloaders

--- app/models/bucket.rb
+++ app/models/bucket.rb
@@ -1,8 +1,7 @@
 class Bucket < ActiveRecord::Base
   include PgSearch
   pg_search_scope :search, :against => [:name, :description]
-  paginates_per 10
-
+  paginates_per 3
   belongs_to :category
   belongs_to :course
   has_many :folders
@@ -10,4 +9,20 @@ class Bucket < ActiveRecord::Base
   has_many :uploads
   has_many :downloads
   has_many :comments, as: :commentable
+
+
+  #TOTALLY wrong implementation
+  #Fix this blunder by me, with dbms design
+  #This implementation allows bucket to have multple uploaders which shouldnt be true
+  def uploader
+    self.uploads.first.user
+  end
+
+
+  def downloaders
+    user_ids = self.downloads.map{|download| download.user }.map(&:id)
+    User.where(:id => user_ids)
+  end
+
+
 end
