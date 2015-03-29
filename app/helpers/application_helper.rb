581ea04992e703f5154ad1b97363deb5b7dcc433
Harsh Trivedi <harshjtrivedi94@gmail.com>
2015-03-30 05:22:37 +0530

    Add support for flash messages in the app

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,4 +17,20 @@ module ApplicationHelper
 		"http://gravatar.com/avatar/#{gravatar_id}.png?"
 	end
 
+
+	def bootstrap_class_for flash_type
+	    case flash_type
+	      when :success
+	        "alert-success"
+	      when :error
+	        "alert-error"
+	      when :alert
+	        "alert-block"
+	      when :notice
+	        "alert-info"
+	      else
+	        flash_type.to_s
+	    end
+	end
+
 end
