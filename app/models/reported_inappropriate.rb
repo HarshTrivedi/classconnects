174bccbf05352a1f80cde45c2ff5bc92cbc1a899
Harsh Trivedi <harshjtrivedi94@gmail.com>
2015-04-13 01:47:57 +0530

    add missing validation in reported_inappropriate model

--- app/models/reported_inappropriate.rb
+++ app/models/reported_inappropriate.rb
@@ -1,9 +1,9 @@
 class ReportedInappropriate < ActiveRecord::Base
-
+	
 	belongs_to :user
 	belongs_to :bucket
 	belongs_to :inappropriate_type
-
+	
 	validates :bucket_id, :uniqueness => { :scope => :user_id }
-
+	
 end
