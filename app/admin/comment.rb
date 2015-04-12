88378d5f27a7fb9e00277c63f35e0e154d951fec
Harsh Trivedi <harshjtrivedi94@gmail.com>
2015-04-13 01:43:54 +0530

    Add show action in Comment model of admin

--- app/admin/comment.rb
+++ app/admin/comment.rb
@@ -1,20 +1,41 @@
 ActiveAdmin.register Comment do
-  menu false
+  # menu false
   permit_params 
 
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:permitted, :attributes]
-  #   permitted << :other if resource.something?
-  #   permitted
-  # end
+  show do
+
+    panel "Thread Title Details" do
+      attributes_table_for comment do
+        row("Name")   { comment.user.name }
+        row("Message"){ comment.message }
+        row("Created")  { time_ago_in_words(comment.created_at)  }
+      end
+    end
+
+    panel "Discussion" do
+      table_for comment.comment_responses do
+        column "User" do |comment_response|
+          user = comment_response.user
+          link_to( user.full_name , admin_user_path( user.id ) )
+        end
+         
+        column "Message" do |comment|
+          comment.message
+        end
+      end
+    end
+
+  end
+
+  sidebar "Go back to Bucket Root", only: [:show ] do
+    
+  end
+
+  sidebar "Any thing can be added here", only: [:show ] do
+    
+  end
+
 
 
 end
