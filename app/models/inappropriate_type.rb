594822dac3633e932680c7f949f4f556e82d3f6c
Harsh Trivedi <harshjtrivedi94@gmail.com>
2015-04-12 12:37:52 +0530

    add missing association for reporting

--- app/models/inappropriate_type.rb
+++ app/models/inappropriate_type.rb
@@ -1,2 +1,5 @@
 class InappropriateType < ActiveRecord::Base
+
+  has_many :reported_inappropriates
+
 end
--- app/models/reported_inappropriate.rb
+++ app/models/reported_inappropriate.rb
@@ -2,5 +2,6 @@ class ReportedInappropriate < ActiveRecord::Base
 
 	belongs_to :user
 	belongs_to :bucket
+	belongs_to :inappropriate_type
 
 end
