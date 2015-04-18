930b120a4209f769b4b15ea767e591f711f4a628
Harsh Trivedi <harshjtrivedi94@gmail.com>
2015-04-18 14:51:55 +0530

    integrate UI updates provided by karan

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -300,14 +300,59 @@ $(document).on('page:load', function() {
 	autocomplete_search_fields();
 	main_college_search_autocomplete();
 	cascade_enroll_college_branch();
+    $('.carousel').carousel({ interval: 15000 })
 });
 
 $(document).on('ready', function() {
 	autocomplete_search_fields();
 	main_college_search_autocomplete();
 	cascade_enroll_college_branch();
+    $('.carousel').carousel({ interval: 15000 })
 });
 
 $(document).on('page:fetch',   function() { NProgress.start(); });
 $(document).on('page:change',  function() { NProgress.done(); });
 $(document).on('page:restore', function() { NProgress.remove(); });
+
+
+
+//circle sidebar
+function setProgress(num){
+  var $spinner = $('.progress-circle .spinner');
+  var $filler = $('.progress-circle .filler');
+  var $percentage = $('.progress-circle .percentage');
+  
+  if (num < 0) num = 0;
+  if (num > 100) num = 100;
+  
+  var initialNum = $percentage.text().replace('%','');
+  $({numVal:initialNum}).animate({numVal:num},{
+    duration: 1000,
+    easing: 'swing',
+    step: function (currVal) {
+      $percentage.text(Math.ceil(currVal) + '%');
+      if (currVal > 0 && currVal <= 50) {
+        $filler.css('display', 'none');
+        var spinnerDegs = -180 + ((currVal * 180) / 50);
+        rotate($spinner, spinnerDegs);
+      } else if (currVal > 50) {
+        rotate($spinner, 0);
+        $filler.css('display', '');
+        var fillerDegs = 0 + ((currVal * 180) / 50);
+        rotate($filler, fillerDegs);
+      }
+    }
+  });
+
+}
+
+function rotate($el, deg) {
+  $el.css({
+    '-webkit-transform': 'rotate(' + deg + 'deg)',
+    '-moz-transform': 'rotate(' + deg + 'deg)',
+    '-ms-transform': 'rotate(' + deg + 'deg)',
+    '-o-transform': 'rotate(' + deg + 'deg)',
+    'transform': 'rotate(' + deg + 'deg)'
+  });
+}
+
