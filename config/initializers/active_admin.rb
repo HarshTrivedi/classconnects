98857799cc54956662a1f1479737bd9094408ded
Harsh Trivedi <harshjtrivedi94@gmail.com>
2015-05-06 00:50:49 +0530

    make sure destry links asks for confirm in active admin

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -54,7 +54,7 @@ ActiveAdmin.setup do |config|
         links += link_to I18n.t('active_admin.edit'), edit_resource_path(resource), :class => "member_link edit_link"
       end
       if can?(:destroy, resource) && controller.action_methods.include?('destroy')
-        links += link_to I18n.t('active_admin.delete'), resource_path(resource), :method => :delete, :confirm => I18n.t('active_admin.delete_confirmation'), :class => "member_link delete_link"
+        links += link_to I18n.t('active_admin.delete'), resource_path(resource), :method => :delete, data: {:confirm => I18n.t('active_admin.delete_confirmation')}, :class => "member_link delete_link"
       end
       links
     end
